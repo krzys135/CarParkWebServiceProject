627ae955b3414069f131417e9f0a2e3fe32fc5f8
krzys135 <krzysztoff.sitarz@gmail.com>
2013-12-07 11:56:36 +0100

    Zmiany

---
+++ src/main/java/com/park/car/service/Resp.java
@@ -0,0 +1,14 @@
+package com.park.car.service;
+
+public class Resp {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
