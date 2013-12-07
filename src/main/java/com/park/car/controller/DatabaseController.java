e5b6f46b7a55d1e370d215fecb7a083bb6ac8b54
krzys135 <krzysztoff.sitarz@gmail.com>
2013-12-07 17:06:14 +0100

    Zmiany

--- src/main/java/com/park/car/controller/DatabaseController.java
+++ src/main/java/com/park/car/controller/DatabaseController.java
@@ -1,9 +1,13 @@
 package com.park.car.controller;
 
+import com.park.car.model.Registration;
+import com.park.car.model.Resp;
 import com.park.car.model.Space;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -49,4 +53,28 @@ public class DatabaseController {
         }
         return mapa;
         }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Resp register(@RequestBody final Registration registration){
+        String sql = "call addUser (null,'"+registration.getEmail()+"', MD5('"+registration.getPassword()+"'), '', '')";
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {}
+            }
+        }
+        return null;
+    }
+
+
 }
